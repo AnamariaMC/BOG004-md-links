fd2b80584b9d02bf677c52376eb2d5d502f0b9b0
AnamariaMC <anymedinac14@gmail.com>
2022-05-09 15:52:34 -0500

    switch con los casos de validación para obtener los stats y el validate

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,53 @@
 #!/usr/bin/env node
-
-const mdLinks = require('./index.js')
-// node methods process
+const mdLinks = require('./index.js');
 const process = require('process');
+const chalk = require('chalk');
+const { arrayTemplate, statusTemplate, totalLinks } = require("./stats.js");
 
-// captura de argumentos de terminal
-const terminalArgs = process.argv;
+const arguments = process.argv.slice(2);
 
-// se llama terminalArguments en ves del array
-mdLinks([ 'C:\\Program Files\\nodejs\\node.exe',
-'C:\\Users\\Ing. Anamaria MC\\Documents\\laboratoria\\BOG004-md-links\\src\\index.js',
-'./mds',
-'--prueba']).then((result)=>{
-    console.log(result)
-  })
-  .catch((error)=>{
-      const errorMessage = 'Error'
-      //reject(error, errorMessage)
-      console.log(error, errorMessage)
-  });
+switch (arguments.length) {
+    case 0:
+        console.log(chalk.redBright.bold('| ✿ PLEASE, ENTER A PATH ✿ |'));
+        break;
+    case 1:
+        mdLinks(arguments[0], { validate: false })
+            .then((response) => {
+                 (console.log(`${arrayTemplate(response)}`));
+            })
+            .catch((err) => console.log(chalk.redBright.bold(err)));
+            break;
+    case 2:
+        //console.log('argumento[1]', arguments[1]);
+        if (arguments[1] === '--validate') {
+            // console.log('argumento[0]', arguments[0]);
+                mdLinks(arguments[0], { validate: true })
+                    .then((response) => {
+                        console.log(`${statusTemplate(response)}`);
+                    })
+                    .catch((err) => console.log(chalk.redBright.bold(err)));
+            } else if (arguments[1] === '--stats') {
+                mdLinks(arguments[0], { validate: true })
+                    .then((response) => {
+                    console.log(`${totalLinks(response)}`);
+                    })
+                .catch((err) => console.log(chalk.redBright.bold(err)));
+            } 
+            // else if (argumentos[1] === '--help') {
+            //   console.log(chalk.cyan.bold(help));
+            // } 
+            else console.log(chalk.redBright.bold('| ✿ INVALID OPTION ✿ |'));
+            break;
+    case 3:
+        if ((arguments[1] === '--validate' && arguments[2] === '--stats') || (arguments[1] === '--stats' && arguments[2] === '--validate')) {
+            mdLinks(arguments[0], { validate: true })
+                .then(response => {
+                console.log(`${totalLinks(response)}`);
+                console.log(`${statusTemplate(response)}`);
+                })
+                .catch((err) => console.log(chalk.redBright.bold(err)));
+        } else console.log(chalk.redBright.bold('| ✿ INVALID OPTIONS ✿ |'));
+        break;
+        default:
+        console.log(chalk.redBright.bold('| ✿ INCORRECT DATA INPUT ✿ |'));           
+}
